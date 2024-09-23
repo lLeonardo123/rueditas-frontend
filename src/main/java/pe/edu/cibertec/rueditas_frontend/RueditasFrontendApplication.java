8cc323b008bdefb466810f954269acc8536c859b
Leonardo Cortarreal <leonardocortarreal123@gmail.com>
2024-09-22 20:55:54 -0500

    Carga inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/rueditas_frontend/RueditasFrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasFrontendApplication.class, args);
+	}
+
+}
